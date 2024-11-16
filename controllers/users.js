c10cdb045a3f9720a275603f6bc99d37cc663596
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-11-16 22:52:57 +0700

    migration db

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,18 @@
 //list logic controller
+const { where } = require('sequelize');
+const { User } = require('../models');
 
-module.exports = {}
+
+const testingUser = async (req,res) => {
+    const find = await User.findOne({
+        where: {
+            name: 'nadini'
+        }
+    })
+    if (!find) {
+        return res.status(400).json({success: false, message: 'Tidak ada data'})
+    }
+    return res.status(200).json({success: true, message: 'Data ada'})
+}
+
+module.exports = {testingUser}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const controllers = require('../controllers/users')
 
 //list router
+router.get('/testing-user', controllers.testingUser)
 
 
 module.exports = router;
